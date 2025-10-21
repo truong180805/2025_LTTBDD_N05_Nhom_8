d28f2475afc4396f036de10629e2d4482e222d0e
truong180805 <nguyentruong180805@gmail.com>
2025-10-21 07:58:16 +0700

    hien thi thoi quen tam thoi

--- mini_habit_challenge/lib/screens/about_screen.dart
+++ mini_habit_challenge/lib/screens/about_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mini_habit_challenge/l10n/app_localizations.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Thong tin"),
+        title: Text(l10n.tabAbout),
       ),
       body: Center(
-        child: Text("Day la trang thong tin"),
+        child: Text(l10n.tabAbout),
       ),
     );
   }
--- mini_habit_challenge/lib/screens/statistics_screen.dart
+++ mini_habit_challenge/lib/screens/statistics_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mini_habit_challenge/l10n/app_localizations.dart';
 
 class StatisticsScreen extends StatelessWidget {
   const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+  
     return Scaffold(
       appBar: AppBar(
-        title: Text("Thong Ke"),
+        title: Text(l10n.tabStatistics),
       ),
       body: Center(
-        child: Text("day la trang thong ke"),
+        child: Text(l10n.tabStatistics),
       ),
     );
   }
