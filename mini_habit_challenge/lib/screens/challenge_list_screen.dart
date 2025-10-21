8623a87393978c79fdff0694c2d2748446d91310
truong180805 <nguyentruong180805@gmail.com>
2025-10-21 07:46:30 +0700

    su dung Consumer trong challenge list de lay du lieu

--- mini_habit_challenge/lib/screens/challenge_list_screen.dart
+++ mini_habit_challenge/lib/screens/challenge_list_screen.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import 'package:mini_habit_challenge/l10n/app_localizations.dart';
+import '../providers/habit_provider.dart';
+
 class ChallengeListScreen extends StatelessWidget {
   const ChallengeListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Thu thach'),
-      ),
-      body: Center(
-        child: Text("Day la trang thu thach"),
-      ),
+    final l10n = AppLocalizations.of(context)!;
+
+    //dung consumer de 'lang nghe' thay doi
+    return Consumer<HabitProvider>(
+      builder: (context, HabitProvider, child) {
+        final habits = HabitProvider.habits; //lay danh sach thoi quen
+
+        return Scaffold(
+          appBar: AppBar(title: Text(l10n.tabChallenges)),
+          body: habits.isEmpty
+              ? Center(child: Text("Chưa có thói quen nào."))
+              : ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: habits.length,
+                  itemBuilder: (context, index) {
+                    final habit = habits[index];
+                    return Card(
+                      elevation: 3,
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      child: ListTile(
+                        title: Text(
+                          habit.name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Text(
+                          "${habit.daysCompleted}/${habit.totalDays}",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        onTap: () {},
+                      ),
+                    );
+                  },
+                ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            tooltip: "Thêm thử thách",
+          ),
+        );
+      },
     );
   }
 }
